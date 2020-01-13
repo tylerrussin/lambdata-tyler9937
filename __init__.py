2b0969f7564af969bfcac5c48f27d01d6c2fdfc3
tyler9937 <tyler9937@users.noreply.github.com>
2020-01-13 15:16:35 -0500

    'test'

---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""
+Lambdata - a collection of data scinece helper functions
+
+"""
+
+import pandas as pd
+import numpy as np
+
+# sample code
+ONES = pd.Dataframe(np.ones(10))
+ZEROS = pd.Dataframe(np.zeros(50))
